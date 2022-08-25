d839f4b4fd258feabc8401cb870b2ff18bb8b730
shreyasdeshmukh <shreyas25deshmukh@gmail.com>
2022-08-25 21:41:52 +0530

    addpopform

---
+++ src/views/addpopup/AddPopUp.js
@@ -0,0 +1,114 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { InputLabel, MenuItem, Select } from '@mui/material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { background } from '@chakra-ui/react';
+import { Box } from '@mui/system';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+export default function AddPopUp() {
+    const [open, setOpen] = React.useState(false);
+    const [gender, SetGender] = React.useState();
+    const [user, setUser] = React.useState({});
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setUser({ ...user, [name]: value });
+    };
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleUserSubmit = () => {
+        console.log(user);
+        fetch('http://192.168.10.21:8080/partner', {
+            method: 'POST',
+            body: JSON.stringify(user),
+            headers: { 'content-type': 'application/json' }
+        })
+            .then(alert('User Data Inserted'))
+            .then((res) => res.json())
+            .then((res) => console.log(res));
+    };
+
+    return (
+        <div>
+            <Button
+                sx={{
+                    padding: '10px',
+                    height: '40px',
+                    minWidth: '40px',
+                    borderRadius: '50%',
+                    background: '#673ab7',
+                    boxShadow: 'rgba(103, 58, 183, 0.3) 0px 19px 38px, rgba(103, 58, 183, 0.22) 0px 15px 12px',
+                    marginBottom: 1,
+                    '&:hover': { backgroundColor: 'black' }
+                }}
+                variant="filled"
+                onClick={handleClickOpen}
+            >
+                <AddCircleIcon sx={{ color: 'white', fontSize: '1.3em' }} />
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle sx={{ fontSize: '1.5em', fontFamily: 'Inter', fontWeight: 600 }}>Add New Entry</DialogTitle>
+                <DialogContentText>you can add new entries here.</DialogContentText>
+                <DialogContent
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}
+                >
+                    <Box
+                        component="form"
+                        sx={{
+                            '& .MuiTextField-root': { m: 1, width: '25ch' }
+                        }}
+                        noValidate
+                        autoComplete="off"
+                    >
+                        <div>
+                            <TextField name="firstName" onChange={handleChange} required id="outlined" label="First Name" />
+                            <TextField name="middleName" onChange={handleChange} required id="outlined" label="Middle Name" />
+                            <TextField name="lastName" onChange={handleChange} required id="outlined" label="Last Name" />
+                            <TextField name="address" onChange={handleChange} required id="outlined" label="Address" />
+                            <TextField name="panNo" onChange={handleChange} required id="outlined" label="PAN No." />
+                            <TextField name="aadharNo" onChange={handleChange} required type="" id="outlined" label="Aadhar No." />
+                            <TextField name="occupation" onChange={handleChange} required type="text" id="outlined" label="Occupation" />
+                            <TextField name="email" onChange={handleChange} required type="email" id="outlined" label="Email" />
+                            <TextField name="dateOfBirth" onChange={handleChange} required type="date" id="outlined" />
+                            <InputLabel id="demo-simple-select-label">Gender</InputLabel>
+                            <Select
+                                sx={{ width: '100px' }}
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={gender}
+                                label="Gender"
+                                onChange={handleChange}
+                                name="gender"
+                            >
+                                <MenuItem value={'Male'}>Male</MenuItem>
+                                <MenuItem value={'Female'}>Female</MenuItem>
+                                <MenuItem value={'Other'}>Other</MenuItem>
+                            </Select>
+                        </div>
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={(handleClose, () => handleUserSubmit())}>Add</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
--- src/views/partners/Partner.js
+++ src/views/partners/Partner.js
@@ -7,6 +7,9 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Button, Modal, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import AddPopUp from 'views/addpopup/AddPopUp';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -28,55 +31,86 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
     }
 }));
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-    return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9)
-];
-
 export default function CustomizedTables() {
     const [data, setData] = React.useState([]);
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const [activateRowOnFocus, setActivateRowOnFocus] = React.useState(true);
 
     React.useEffect(() => {
         fetch('http://192.168.10.21:8080/partner/1')
             .then((res) => res.json())
             .then((res) => setData([...data, res]));
-    }, [data]);
+    }, []);
 
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 700 }} aria-label="customized table">
-                <TableHead>
-                    <TableRow>
-                        <StyledTableCell>FIELDS</StyledTableCell>
-                        <StyledTableCell align="right">VALUES</StyledTableCell>
-                        {/* <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-                        <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-                        <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell> */}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {data.map((partner) =>
-                        Object.keys(partner).map((row, index) => (
-                            <StyledTableRow key={row.name}>
-                                <StyledTableCell component="th" scope="row">
-                                    {row}
+        <>
+            <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: 400,
+                        bgcolor: 'background.paper',
+                        borderRadius: 2,
+                        boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
+                        p: 4
+                    }}
+                >
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                        more information
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        below you can view your other details.
+                    </Typography>
+                </Box>
+            </Modal>
+            <AddPopUp />
+
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>ID</StyledTableCell>
+                            <StyledTableCell>Name</StyledTableCell>
+                            <StyledTableCell>Gender</StyledTableCell>
+                            <StyledTableCell>Status</StyledTableCell>
+                            <StyledTableCell align="right">More Info</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map((partner) => (
+                            <StyledTableRow key={partner.uniqueId}>
+                                <StyledTableCell>{partner.id}</StyledTableCell>
+                                <StyledTableCell>{partner.firstName}</StyledTableCell>
+                                <StyledTableCell>{partner.gender}</StyledTableCell>
+                                <StyledTableCell>{partner.status}</StyledTableCell>
+                                <StyledTableCell align="right">
+                                    <Button
+                                        onClick={handleOpen}
+                                        sx={{
+                                            backgroundColor: '#e3f2fd',
+                                            color: '#616161',
+                                            boxShadow: 'none',
+                                            '&:hover': {
+                                                color: '#ffffff',
+                                                boxShadow: 'none'
+                                            }
+                                        }}
+                                        align="right"
+                                        variant="contained"
+                                    >
+                                        more info.
+                                    </Button>
                                 </StyledTableCell>
-                                <StyledTableCell align="right">{partner[row]}</StyledTableCell>
-                                {/* <StyledTableCell align="right">{row.fat}</StyledTableCell> */}
-                                {/* <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-                            <StyledTableCell align="right">{row.protein}</StyledTableCell> */}
                             </StyledTableRow>
-                        ))
-                    )}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
     );
 }
